fa0709e7a242cf1397105adcdd6522f573bfba55
Hariom Patil <143918040+hariom9617@users.noreply.github.com>
2025-11-05 12:53:20 +0530

    Update Home component layout and structure
    Refactor Home component to include Navbar, Header, and Footer.

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import Login from './Login'
+import Navbar from '../Component/Navbar'
+import Header from '../Component/Header'
+import Footer from '../Component/Footer'
 
 const Home = () => {
   return (
-    <div>
-      This is home page
-      
-      <Login></Login>
-    </div>
+ <div className="min-h-screen bg-white">
+  <Navbar />
+
+  <div className="mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-32">
+    <Header />
+    <Footer />
+  </div>
+</div>
+
   )
 }
 
-export default Home
+export default Home;
